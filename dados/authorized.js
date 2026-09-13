b9511154d6eed17e392114110984b1b5ee2d00a5
Kviktor10 <117000550+Kviktor10@users.noreply.github.com>
2026-09-13 18:35:36 -0300

    Create authorized.js

---
+++ dados/authorized.js
@@ -0,0 +1,115 @@
+/* GERADO OFFLINE — telefones não ficam em texto puro. */
+window.AUTHORIZED = {
+  "version": 2,
+  "lookupSalt": "curso-2026-public-lookup-v2-9d4e71",
+  "deriveSalt": "curso-2026-public-derive-v2-a81f03",
+  "pbkdf2Iterations": 180000,
+  "sessionHours": 8,
+  "records": [
+    {
+      "hash": "0539aa92ab4fa7adf0585f177f673c25ec58b8ab8c8eb9c7fd5614b3951b0fd3",
+      "iv": "KVBFU7+VzmTXALmq",
+      "ciphertext": "WAPQ37ZKgZzRjlClZb+NeXhwnaYnNY/bpcyo6V7fZYAA4k2PMIPK7lFldmawQ9mcQhXaxP57Bu1mBP9aTrE0VpgfQ/0OvgeJfvu7cVrJtGG8A93zlA=="
+    },
+    {
+      "hash": "a79f1d4655065141caf111d02f25ce1f763bc4f0a2825fc48a4411401a968078",
+      "iv": "YE0m3nIOx5aoAnhO",
+      "ciphertext": "J7tpqY+0GXoylrBXjrA+iWPYgArtp++DO0z6Rf+2eXnT6lT43YKY/Jnz7qpmONfUo6smaIuTJtZT1feb8488dvg0hWxCmqMtILRmD5Sn8yLFOhIm9Q=="
+    },
+    {
+      "hash": "3e21e32bc52ad050d12a5567988d0aa834e5fa383f1d3f33bef2e1a055bfa5ed",
+      "iv": "w93qSlOm3nc4SSiR",
+      "ciphertext": "rztF2JKAszDcPvdQjMmXsKAXeQ8sfcCG4ElDdQcAjg5T5L+7OIYapE5iiAcuku8Oj7lf/nfRX6lOnAzFYf5/SUS8SlpZnlxfbbF8DwaRev7HvrFcbg=="
+    },
+    {
+      "hash": "b5126b24cc6908b97f9bc34568cadaf0abeae006f329ec6c89ae274e03038544",
+      "iv": "HAPu3qmTr+ntqiSj",
+      "ciphertext": "1opqlLIeiRzaLipynLfbMFKjDieko3YCZ5wPLHD64vMDWX/OHZjEb4Gk7L+kD7Z5ZzLi3GWcFqKWK10vaj/VIWih9DqCmw67jmV4sodzrl9zQBQYlA=="
+    },
+    {
+      "hash": "07760810e64d9d9ffdda41672e2af63f27f530b10548858d764255bea5bd68df",
+      "iv": "0q9y7fbaMEDAh2qU",
+      "ciphertext": "q+5CtdSvzfqETUqBTMzRauX1xYubJXPk51bK67OhKnWTLtXWkcPOtYyNaHIIHztfLwFjBdyrOayJH4QMHhmTnJgQXF39DAt4yKOaQ0YLlFzgUAlKog=="
+    },
+    {
+      "hash": "ea1fb19dad95bb0532d6a0efaeae73771074af4041a6008b76058af86420ba2d",
+      "iv": "AfWFoGDksXFVS1hR",
+      "ciphertext": "bj+vNinw9KNuUBdXvuBNRFdOIdQ8U3aPniNfSIw6AVde+q0ZO9ZJcGitw8M0G9yVcIo3r5FAptkT0uSaSm4WpANR0st2MCnzC1kwYuDDDWzFmPRO+A=="
+    },
+    {
+      "hash": "c1b21c8cb6c1093edea5de65b2f9c03f9993f553be46f5952ad4e27831844001",
+      "iv": "Z8dcMcUT13bX4Zr0",
+      "ciphertext": "QnSnerAIQE4mLAEQQlSHGIbmA7WGtLly8kWjeISo3w3Xy6wX8c4af5pfKFFBvRfPJjk8ox+X+n10pi8v5haesmJNZ5qn+PNTWf8Abe4GZ9ECwnlzDw=="
+    },
+    {
+      "hash": "fd4ed4862839c9371b03af5fc836e127c945a85a82c3d47c3b7eef0e204eef22",
+      "iv": "SMX35Qy8rWWF5zWN",
+      "ciphertext": "UbjFxANcCWmEto7UqlS2UANPw+NjRVwGYuu6azdCJcuxxBW6/EIRDNkYplPtm4lPpj5hgfUJHM7veDtXSfbADFQcetWVcEttNlIFUIwlYIr/St6Aqw=="
+    },
+    {
+      "hash": "29e4a655c6b6c72af94e6a9117b732f76075c8e87c32a9cba543e508df04b607",
+      "iv": "HLquvSqIVADWFkzu",
+      "ciphertext": "nXRKP47walAINwNnxEGsSFvrBXgqRgKOaFlfigvQIf8pz9YfRmpqxKlGkfeytt1Ot7W4q+9jyGhhYL1EHOYpJD2hanxIQUBUoGeHVvcE20jnhiPxiw=="
+    },
+    {
+      "hash": "af4194ffa38f9f4f8032dbe5096c2542830f5b8b143e844df5a44aa81e89ad45",
+      "iv": "nfckKyvIG4ytIrVq",
+      "ciphertext": "QobLiY0omTkPH+rmemWju+2+7rQgWpaqvawL9nwfKcVneJ2QnPkj8sVfZZfvRnnG9XqSk2YeWP5da80PDYUqffIfvg02/750nkqsVXBmXHoeQ//dVA=="
+    },
+    {
+      "hash": "ddaa4f604ca96c2bd15e0dd8fbf763771b459c161e82ccc51e7aaacd4239a041",
+      "iv": "F+kwwoKym+FTGIYh",
+      "ciphertext": "bTxnCU9CNJqZw4/GL5my2PPoarcJLznqXmTxpKWPimRgO2Z1RlqY6WElmwRhZG1gytmmlOQ1Ra8s3aEmRo8TmXdcUjvYzOLVFvJhOZnoLm97mQ4q9A=="
+    },
+    {
+      "hash": "c77e90a1070eed8d90a6636f45a7628b14bd920d9d3288957d79dbab13df8df7",
+      "iv": "UfpmHEDvP9DKVtWn",
+      "ciphertext": "AttB//ugoQyYCoa4DlI3TSOlnVEFH69S+QCWipuHWk93MJrdkVW1JfvVL9H15nEfJctXVW+MnLVh4kIMUzfrzuN1BVhqHLs7VIVY35XJ5exTRdOl/A=="
+    },
+    {
+      "hash": "ffde1bf0c9a07b439facf702a6c98a540edb97e69743f4748134b3bad6400e05",
+      "iv": "FKp9BvZZAJHS+zEP",
+      "ciphertext": "h9hOap1WfxvzrL4WmGexgI95QsHIrEQfCFEm0UrEwIVKgaJolu+4YzkakDSX13wm7x4LQr2y8ABNfRSxHjOfXFXlAxtuDTVcKa3XhiTNs3VMGYn1fA=="
+    },
+    {
+      "hash": "ada717762fb4a3a9952d2ad0d255cbb01caba15876362f65532993bf1d51df61",
+      "iv": "u+O+w/vRsAdRSYWO",
+      "ciphertext": "6uvHgERA+z5FRDUx/iRIRmwha1bSHU8vf2rmjo1gsboKEvkxsNh8e9YYL+2aiWJGC8yi4SH0olHbzl5NdlRMAtJWlx3IeZDm4mID8b53IdHJX46nEg=="
+    },
+    {
+      "hash": "2d8852166f2b6d5b69f6ac738c4442c53f21a97b0213c748151a65cdfcd516b9",
+      "iv": "pc4VOAks+ZJqaUIu",
+      "ciphertext": "FTk/BsZdYBfQPBvPVQozlQaW64mxfvyOS20iGgQYJHWtR2WmvDjdGrAouZ66j9JrhiGIIFLD2aN2W0uwJDzhQNHbQteXTPXK+XYQlPsSKDfprdSwsA=="
+    },
+    {
+      "hash": "016992c8b6d89117c35934b7bd87708641ae0a13078e4a9daf25a02807232007",
+      "iv": "braaDj5OycHw3tGq",
+      "ciphertext": "yNZCd4nurFUUpzgqcl41QFaCE+BJYQ0wBRqM+XJtOsXyIpQwGK4M04oa1UcjbzsYAouYleQRz8yAxHlOSgRp+7kkOlY/QtkADl9L0LmYyOeYIX21YQ=="
+    },
+    {
+      "hash": "a9360afbc859db17ccafb9bb2defb99af2eb60fcfa88fa7de49d980f861a490e",
+      "iv": "k277jivkrekkHr52",
+      "ciphertext": "xxnfF8Y72V1nlz6+THyOwDTm7utq3hwYX3GpDNAt/j2aGmCf4kUoq7+ZdQ3D4mLcZS44DsRDWS+m/+Up3STSNsX1eYz2hN4aDUM5APN5+75YRDBATw=="
+    },
+    {
+      "hash": "895b0daf06d449b5beb1ed4118abde11a7ef2257fd120f05c08c1076a9214a5d",
+      "iv": "jk5NuY4wXWEpb0yd",
+      "ciphertext": "bWdw8ch1/5XF5DO7iLNN1ezxnUlI1TSNWtdDPWzdsmY/nx+6s7sk/xjzt3VTsRwLHYe1Yw8ANY1Hnpznhc/xisXiTMXe0JRh0BbX4suD9NxiawVodg=="
+    },
+    {
+      "hash": "685c0b4621c1a49beabce9e5cc9ffa6585f6a68c3f49366367357edd1962d935",
+      "iv": "lujNSfbXqMpa/mpf",
+      "ciphertext": "rpCZgxKQHUrGv9oGM5rUfHuQp8IQ8TMe1ujujzgZit+dlo6JCpgWZPsC0PNz+J/3HvMt/i2xGbOZMm+mRYPxQvSyzDFAyKFaXtrzCGefIfpsN5KrPg=="
+    },
+    {
+      "hash": "4f1a41a08be1f410419dbbae89c20e8bb792e1a75192ffa6bb87323ab4179e28",
+      "iv": "DjLAbnvh8SnIPIlU",
+      "ciphertext": "aJiyZ4WEI/Got2L6FRcroI7PEmg6LorSi7Kdzb0Jhtvpswrvi5k/kFAHOFQlrCKZ14cY0+xYCfSigJ1IO5QNEpow4Cjfg1B4Qinn/fy0ZX6WyhEjjA=="
+    },
+    {
+      "hash": "6cfdd8740cd7efd74020f97b5f80a0b65325fda67e173bf2d2c089deba8abe22",
+      "iv": "X1kwycZdgObLNrtS",
+      "ciphertext": "KsNqw5mQXMtLybpuVz+jLPa9eNpsE5nW9oA3lY8iHTHIiXPXzTiAaoaUIee7RnJB54a90DefH6B5CNs254gJNE7CManS1rL89jaC0uXXOpLiouCDgQ=="
+    }
+  ]
+};
